05b6967013be5415fbc62f1cf0d6219b1169a0ae
Jora <handavz@gamil.com>
2026-05-19 15:30:53 +0300

    start project

---
+++ src/main/java/com/example/Zhora/ZhoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zhora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Zhora/ZhoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zhora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
